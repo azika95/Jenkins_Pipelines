a53b7df7a9cadcdede79f9e9b15a622322aa708c
azizasalieva95 <azika1695@gmail.com>
2020-02-08 12:45:26 -0600

    edited

---
+++ Jenkinsfile_for_EKS.groovy
@@ -0,0 +1,15 @@
+node{
+    stage("pull repo"){
+        git 'https://github.com/bkarakas/terraform-iaac-eks-burak.git'
+
+    }
+    stage("stage1"){
+        echo "Hello"
+    }
+    stage("stage1"){
+        echo "Hello"
+    }
+    stage("stage1"){
+        echo "Hello"
+    }
+}
