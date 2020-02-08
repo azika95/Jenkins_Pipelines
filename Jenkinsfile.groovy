b9b4b6b7ec635ee2c14f432eac1330bbc9d853d0
azizasalieva95 <azika1695@gmail.com>
2020-02-08 11:37:10 -0600

    edited

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,18 +1,19 @@
 node {
-    stage ("Pull Repo"){
-        echo  "Hello"
+    properties([parameters([
+        choice(choices: ['golden_ami', 'tower', 'elk', 'nagiosxi', 'gitlab', 'nexus', 'vault'], description: 'What tool would you like to build?', name: 'TOOL_TO_PROVISION'), 
+        choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Please choose a region', name: 'AMI_REGION')])])
 
+    stage("Pull Repo"){
+        git 'https://github.com/azizasalieva95/Jenkins_Pipelines.git'
     }
-    stage ("Build Image"){
-        sh "Packer version"
-        
+    stage("Build Image"){
+        sh "packer version"
+        //sh "packer  build -var region=${AMI_REGION} tools/jenkins_example.json"
     }
-    stage ("Send Notifications to Slack"){
-        echo "Hello"
-        
+    stage("Send Notification to Slack"){
+        slackSend channel: 'nagios_alerts', message: 'Golden AMI has been built'
     }
-    stage ("Send Email"){
-        echo "Hello"
-        
+    stage("Send Email"){
+        mail bcc: '', body: '''Hello, Your AMI is ready in us-east-1 Thanks''', cc: '', from: '', replyTo: '', subject: 'Golden AMI has been built', to: 'farrukh@gmail.com'
     }
 }
