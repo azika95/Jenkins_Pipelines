804e0ad75261a227b5bb61442a3c4cb5b4a38828
azizasalieva95 <azika1695@gmail.com>
2020-02-08 13:19:51 -0600

    edited

--- Jenkinsfile_for_EKS.groovy
+++ Jenkinsfile_for_EKS.groovy
@@ -1,15 +1,18 @@
 node{
-    stage("pull repo"){
-        git 'https://github.com/azizasalieva95/Jenkins_Pipelines.git'
+    stage("Pull Repo"){
+        git 'https://github.com/farrukh90/terraform-iaac-eks-burak.git'
+    }
+    stage("Download Terraform"){
+        sh "terraform version"
+        sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
+        sh "unzip terraform_0.12.19_linux_amd64.zip"
+        sh "./terraform version"
+    }
+    stage("stage1"){
+        echo "Hello"
+    }
+    stage("stage1"){
+        echo "Hello"
 
-    }
-    stage("stage1"){
-        echo "Hello"
-    }
-    stage("stage1"){
-        echo "Hello"
-    }
-    stage("stage1"){
-        echo "Hello"
     }
 }
